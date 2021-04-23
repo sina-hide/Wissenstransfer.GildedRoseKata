ed46c17e82669e9a716f3f8c3ad862577a640544
Sina Hinderks <sina@hinderks.net>
2021-04-23 14:53:19 +0200

    format code

--- GildedRose.Console/Program.cs
+++ GildedRose.Console/Program.cs
@@ -10,33 +10,32 @@ namespace GildedRose.Console
         }
 
         IList<Item> Items; // Don't change this field!
+
         static void Main(string[] args)
         {
             System.Console.WriteLine("OMGHAI!");
 
             var app = new Program()
-                          {
-                              Items = new List<Item>
-                                          {
-                                              new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                                              new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                                              new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                                              new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                                              new Item
-                                                  {
-                                                      Name = "Backstage passes to a TAFKAL80ETC concert",
-                                                      SellIn = 15,
-                                                      Quality = 20
-                                                  },
-                                              new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
-                                          }
-
-                          };
+            {
+                Items = new List<Item>
+                {
+                    new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 },
+                    new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 },
+                    new Item { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7 },
+                    new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 },
+                    new Item
+                    {
+                        Name = "Backstage passes to a TAFKAL80ETC concert",
+                        SellIn = 15,
+                        Quality = 20
+                    },
+                    new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6 }
+                }
+            };
 
             app.UpdateQuality();
 
             System.Console.ReadKey();
-
         }
 
         public void UpdateQuality()
@@ -147,5 +146,4 @@ namespace GildedRose.Console
 
         public int Quality { get; set; }
     }
-
 }
