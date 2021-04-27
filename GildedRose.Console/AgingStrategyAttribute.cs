f5030ca008ee377668fb037e9f481f3aad1fce66
Sina Hinderks <sina@hinderks.net>
2021-04-27 17:36:08 +0200

    mark aging strategies as implicitly used
    Since we also have an attribute, we use [MeansImplicitUse] for the
    attribute AgingStrategyAttribute.  Then ReSharper/Rider doesn't flag our
    Aging Strategies as unused.

--- GildedRose.Console/AgingStrategyAttribute.cs
+++ GildedRose.Console/AgingStrategyAttribute.cs
@@ -1,8 +1,10 @@
 using System;
+using JetBrains.Annotations;
 
 namespace GildedRose.Console
 {
     [AttributeUsage(AttributeTargets.Class)]
+    [MeansImplicitUse]
     public class AgingStrategyAttribute : Attribute
     {
         public bool IsDefault { get; set; } = false;
